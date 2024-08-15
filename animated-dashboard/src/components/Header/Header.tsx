7e93271fc3144675b6887c9fc3685cdf59b6d03d
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-08-15 18:32:59 +0530

    ......

--- animated-dashboard/src/components/Header/Header.tsx
+++ animated-dashboard/src/components/Header/Header.tsx
@@ -21,20 +21,18 @@
 
 
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 import { IoMdMenu } from 'react-icons/io';
 import './Header.css';
 
-const Header: React.FC<{ section?: string }> = ({ section }) => {
+const Header: React.FC = () => {
   const [Menu, setMenu] = useState(false);
 
   const toggleMenu = () => {
     setMenu(!Menu);
   };
 
-  const location = useLocation();
-  const currentSection = section || location.pathname.split('/')[1] || 'section1';
 
   return (
     <header className="header">
@@ -49,21 +47,20 @@ const Header: React.FC<{ section?: string }> = ({ section }) => {
         </button>
       </div>
       {Menu && (
-        <div className="bg-white w-56 md:hidden block">
-          <div className="flex">
-            {/* <ul className="text-center">
-              <li className="p-3"> */}
-                <Link to="/section1">Section 1</Link>
-              {/* </li>
-              <li className="p-3"> */}
-                <Link to="/section2">Section 2</Link>
-              {/* </li>
-              <li className="p-3"> */}
-                <Link to="/section3">Section 3</Link>
-              {/* </li> */}
-            {/* </ul> */}
-          </div>
+        <div>
+
+            <ul className="link">
+            
+          <li><Link to="/section1">Section 1</Link></li>
+          <li><Link to="/section2">Section 2</Link></li>
+          <li><Link to="/section3">Section 3</Link></li>
+        
+            </ul>
         </div>
+         
+         
+        
+        
       )}
     </header>
   );
